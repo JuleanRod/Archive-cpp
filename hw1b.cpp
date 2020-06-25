8366f52835a4702cf0e690afb8ebde8397010ecd
Julian <92952502+juleanrod@users.noreply.github.com>
2020-06-24 17:57:01 -0700

    first cpp program

---
+++ hw1b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long double apple;
+
+    cout << "The size of a char is " << sizeof(char);
+    cout << " bytes.\n";
+    cout << "The size of an integer is " << sizeof(int);
+    cout << " bytes.\n";
+    cout < "The size of a float is " << sizeof(float);
+    cout << " bytes.\n";
+    cout << "The size of a double is " << sizeof(double);
+    cout << " bytes.\n";
+    return 0;
+}
